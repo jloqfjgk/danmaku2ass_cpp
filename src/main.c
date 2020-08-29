5861381c9769fb9e7e9c3fd8e4e513d7573e919b
YK Liu <cos.lyk@gmail.com>
2020-08-29 17:30:58 +0200

    Fix memory error

--- src/main.c
+++ src/main.c
@@ -130,7 +130,7 @@ int main(int argc, char **argv)
 
     if (output == NULL)
     {
-        output = (char*) malloc(strlen(input) + 4);
+        output = (char*) malloc(strlen(input) + 5);
         strcpy(output, input);
         strcat(output, ".ass");
     }
